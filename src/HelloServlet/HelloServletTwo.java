73093d519bdb527f72a31364f1bb21bc87a750b6
ivan4000 <sxln1@mail.ru>
2018-12-10 20:30:35 +0300

    JEE1-5 servlets

--- src/HelloServlet/HelloServletTwo.java
+++ src/HelloServlet/HelloServletTwo.java
@@ -1,18 +1,17 @@
 package HelloServlet;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/hello")
-public class HelloServlet  extends HttpServlet {
+public class HelloServletTwo extends HttpServlet{
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter printWriter= resp.getWriter();
-        printWriter.write("Hello Servlet!!!");
+        PrintWriter printWriter=resp.getWriter();
+        printWriter.write("This is second servlet");
     }
 }
